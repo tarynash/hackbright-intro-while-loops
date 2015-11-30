beded67d34af7ac3c2b02e8685d353580a83601b
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-11-29 17:10:48 -0800

    blastoff

--- 01-while-loops/while_fun_loops.py
+++ 01-while-loops/while_fun_loops.py
@@ -16,3 +16,24 @@
 #	print i,
 #	i += 10
 
+#i = 0
+#while(i<len(range(0,10))):
+#	if(i%2 == 0):
+#		i += 1
+#	else:
+#		print i,
+#		i += 1
+
+#i = 10
+#while i >= 0:
+#	print i
+#	i -= 1
+
+#i = 10
+#while(i>=0):
+#	if i == 0:
+#		print "Blastoff!"
+#		i -= 1
+#	else:
+#		print i
+#		i -= 1
